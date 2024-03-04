f87cafa45a62b6b3018aefecc70136bc9e661634
inhowe <inhowe@qq.com>
2024-03-05 03:29:30 +0800

    优化

--- raspberry_os_lite/aria2/install_aria2_ariang.sh
+++ raspberry_os_lite/aria2/install_aria2_ariang.sh
@@ -7,9 +7,9 @@ sudo cp $(dirname $0)/aria2.session /home/pi/.config/aria2/aria2.session
 sudo cp $(dirname $0)/aria2.service /lib/systemd/system/aria2.service
 
 #aria2c --conf-path=/home/pi/.config/aria2/aria2.config
-#sudo systemctl daemon-reload
-#sudo systemctl enable aria2
-#sudo systemctl start aria2
+sudo systemctl daemon-reload
+sudo systemctl enable aria2
+sudo systemctl start aria2
 sudo systemctl status aria2
 
 echo "Install nginx"
@@ -27,6 +27,7 @@ sudo unzip AriaNg-1.3.7.zip -d ariang
 sudo rm -rf AriaNg-1.3.7.zip
 sudo rm -rf /var/www/html/ariang
 sudo cp -r ariang /var/www/html/ariang
+sudo rm -rf ariang
 
 echo "AriaNG访问地址：raspberrypi.local/ariang" >> Read_Me_After_Install.txt
 echo "Aria2 RPC 密钥：secret" >> Read_Me_After_Install.txt
--- raspberry_os_lite/general/general.sh
+++ raspberry_os_lite/general/general.sh
@@ -3,7 +3,7 @@ sudo cp $(dirname $0)/config.txt /boot/firmware/config.txt
 
 echo "Install command"
 sudo apt-get install -y vim
-sudo apt-get install -y git 
+sudo apt-get install -y git
 sudo apt-get install -y build-essential
 sudo apt-get install -y cmake
 sudo apt-get install -y tmux
@@ -16,4 +16,9 @@ sudo apt-get install -y tig
 sudo apt-get install -y ntfs-3g
 sudo apt-get install -y exfat-fuse
 sudo apt-get install -y lrzsz
+sudo apt-get install -y iotop
+sudo apt-get install -y ack
+sudo apt-get install -y iftop
+sudo apt-get install -y exa
+sudo apt-get install -y duf
 sudo modprobe fuse
